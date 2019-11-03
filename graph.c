065a5c6c068a1e6fc692f7bf8c85a048a226ee4a
Pedro Haluch <pedro.haluch@gmail.com>
2019-11-03 18:35:14 -0300

    finalizando construção de função de geração aleatória de grafos

--- graph.c
+++ graph.c
@@ -3,68 +3,107 @@
 #include <stdlib.h>
 
 
-// Creating a graph with N nodes
-
+// Definindo struct de grafo
 typedef struct graph_structure graph;
 
 struct graph_structure{
-    int n_vertices;
-    int n_links;
-    adjacents adjacency_list[]; 
+    int n_vertices;  // Número de vértices
+    int n_links;  // Número de arestas 
+    adjacents adjacency_list; // Vetor de listas ligadas, de adjacência
 };
 
-int create_graph(int n_vertices, float density)
+float random_weight(int max_weight)
 {
+    // Função para retornar pesos aleatórios aos vértices do grafo.
+    if(max_weight == 0) return 1;
+    return 1 + (rand() % max_weight);
+}
+
+graph create_graph(int n_vertices, float density, int max_weight)
+{
+    // Função que retorna struct 'grafo' preenchida com n_vertices e densidade de links = 'density'
     int i;
-    graph g = malloc(sizeof(*g));
-    g->n_vertices = n_vertices;
-    g->n_links = 0;
-    g->adjacency_list = create_list();
-    
+    graph g = malloc(sizeof(*g));  // Aloca espaço na memória para struct de grafo
+    g->n_vertices = n_vertices;  // Pré-determina número de vértices
+    g->n_links = 0;  // Número de links é zero porque é incrementado dinamicamente
+    g->adjacency_list = (adjacents*)malloc(sizeof(adjacents)*n_vertices);
+    // Cria vetor com tamanho 'v_vertices' de ponteiros para listas de adjacências.
     for(i=0; i<n_vertices; i++)
     {
-
+        // Inicializa cada elemento to vetor de listas ligadas
+        g->adjacency_list[i] = create_list();  // Ponteiro para lista de adjacência
+        g->adjacency_list[i]->len = 0;  // Comprimento zero
+        g->adjacency_list[i]->label = i;  // Label igual ao índice do vetor
     }
-    // Início do grafo, condição mínima de existência
-    for(i=0; i<grafo->n_vertices-1; i++)
+
+
+    // Inicialização do grafo, condição mínima de existência
+    // Fazendo uma 'fila indiana' de conexões, onde 0 está ligado a 1, 1 a 2, 2 a 3, etc.
+    // Se isso não for garantido o grafo fica fatiado em vários mini-grafos não contínuos.
+
+    for(i=0; i < grafo->n_vertices; i++)
     {
         switch(i)
         {
-            case 0:
-                grafo->ad_list[i].push_in_front(vertice[value=i+1])
-                ligacoes++;
+            case 0:  // Caso inicial, onde não há elemento anterior a adicionar
+                /*
+                Esse bloco de código adiciona, na lista de adjacência do vértice 0, o vértice
+                1 como elemento.
+                */
+                next_label = i+1;
+                next_value = i+1;
+                next_weight = random_weight(max_weight);
+                add_adjacent(next_label, next_value, next_weight, grafo->adjacency_list[i]);
+                grafo->n_links++;
+                break;
+
             case grafo->n_vertices-1:
-                grafo->ad_list[i].push_in_front(vertice[value=i-1])
-                ligacoes++;
+                /*
+                Esse bloco de código adiciona, na lista de adjacência do último vértice do vetor
+                de listas de adjacência , o penúltimo vértice como elemento.
+                */
+                next_label = i+1;
+                next_value = i+1;
+                next_weight = random_weight(max_weight);
+                add_adjacent(next_label, next_value, next_weight, grafo->adjacency_list[i]);
+                grafo->n_links++;
+                break;
+
             default: 
-                grafo->ad_list[i].push_in_front(vertice[value=i+1])
-                ligacoes++;
-                grafo->ad_list[i].push_in_front(vertice[value=i-1])
-                ligacoes++;
+                /*
+                Essa opção default se aplica a todos os vértices que não são o primeiro e o último.
+                Ela adiciona tanto o vértice anterior como o próximo vértice na sequência como 
+                elementos da lista de adjacẽncia.
+                */
+                next_label = i+1;
+                next_value = i+1;
+                next_weight = random_weight(max_weight);
+                add_adjacent(next_label, next_value, next_weight, grafo->adjacency_list[i]);
+                grafo->n_links++;
+
+                prev_label = i-1;
+                prev_value = i-1;
+                prev_weight = random_weight(max_weight);
+                add_adjacent(prev_label, prev_value, prev_weight, grafo->adjacency_list[i]);
+                grafo->n_links++;
         }
     }
 
-    if(densidade_alvo == 0)
+    if(density == 0)
     {
-        return grafo
+        // Se a densidade alvo for 0, ou seja, a menor possível, o grafo já está pronto.
+        return grafo;
     }
-
-    while(ligacoes/lig_max <= densidade_alvo)
+    int max_links = (grafo->n_vertices)*(grafo->n_vertices - 1);  // Definindo valor máximo de links
+    float current_density = grafo->n_links/max_links;  // Valor atual da densidade (links_atual/max)
+    while(current_density <= density)
     {
-        p1 = gerar_peso();
-        p2 = gerar_peso();
-        v1 = v_aleatorio();
-        v1->peso = p1;
-        v2 = v_aleatorio();
-        v2->peso = p2;
-        if(graph[v1].contains(v2) || v1==v2)
-        {
-            continue;
-        }
+        // Pegando dois elementos aleatórios do grafo, pelos labels
+        vertice_1 = rand() % grafo->n_vertices;
+        vertice_2 = rand() % grafo->n_vertices;
+
+        // Testando para ver se eles já não estão ligados
         
-        push_at_start(v2, graph[v1])
-        push_at_start(v1, graph[v2])
-        ligacoes++;
     }
 
 }
