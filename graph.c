1b14d86dc137ff80011a085286a41e160b972444
Pedro Haluch <pedro.haluch@gmail.com>
2019-11-03 18:52:26 -0300

    finalizada criação da função

--- graph.c
+++ graph.c
@@ -41,7 +41,7 @@ graph create_graph(int n_vertices, float density, int max_weight)
     // Fazendo uma 'fila indiana' de conexões, onde 0 está ligado a 1, 1 a 2, 2 a 3, etc.
     // Se isso não for garantido o grafo fica fatiado em vários mini-grafos não contínuos.
 
-    for(i=0; i < grafo->n_vertices; i++)
+    for(i=0; i < g->n_vertices; i++)
     {
         switch(i)
         {
@@ -53,11 +53,11 @@ graph create_graph(int n_vertices, float density, int max_weight)
                 next_label = i+1;
                 next_value = i+1;
                 next_weight = random_weight(max_weight);
-                add_adjacent(next_label, next_value, next_weight, grafo->adjacency_list[i]);
-                grafo->n_links++;
+                add_adjacent(next_label, next_value, next_weight, g->adjacency_list[i]);
+                g->n_links++;
                 break;
 
-            case grafo->n_vertices-1:
+            case g->n_vertices-1:
                 /*
                 Esse bloco de código adiciona, na lista de adjacência do último vértice do vetor
                 de listas de adjacência , o penúltimo vértice como elemento.
@@ -65,8 +65,8 @@ graph create_graph(int n_vertices, float density, int max_weight)
                 next_label = i+1;
                 next_value = i+1;
                 next_weight = random_weight(max_weight);
-                add_adjacent(next_label, next_value, next_weight, grafo->adjacency_list[i]);
-                grafo->n_links++;
+                add_adjacent(next_label, next_value, next_weight, g->adjacency_list[i]);
+                g->n_links++;
                 break;
 
             default: 
@@ -78,24 +78,24 @@ graph create_graph(int n_vertices, float density, int max_weight)
                 next_label = i+1;
                 next_value = i+1;
                 next_weight = random_weight(max_weight);
-                add_adjacent(next_label, next_value, next_weight, grafo->adjacency_list[i]);
-                grafo->n_links++;
+                add_adjacent(next_label, next_value, next_weight, g->adjacency_list[i]);
+                g->n_links++;
 
                 prev_label = i-1;
                 prev_value = i-1;
                 prev_weight = random_weight(max_weight);
-                add_adjacent(prev_label, prev_value, prev_weight, grafo->adjacency_list[i]);
-                grafo->n_links++;
+                add_adjacent(prev_label, prev_value, prev_weight, g->adjacency_list[i]);
+                g->n_links++;
         }
     }
 
     if(density == 0)
     {
         // Se a densidade alvo for 0, ou seja, a menor possível, o grafo já está pronto.
-        return grafo;
+        return g;
     }
-    int max_links = (grafo->n_vertices)*(grafo->n_vertices - 1);  // Definindo valor máximo de links
-    float current_density = grafo->n_links/max_links;  // Valor atual da densidade (links_atual/max)
+    int max_links = (g->n_vertices)*(g->n_vertices - 1);  // Definindo valor máximo de links
+    float current_density = g->n_links/max_links;  // Valor atual da densidade (links_atual/max)
 
     while(current_density <= density)
     /*
@@ -105,19 +105,38 @@ graph create_graph(int n_vertices, float density, int max_weight)
     */
     {
         // Pegando dois elementos aleatórios do grafo, pelos labels
-        vertice_1 = rand() % grafo->n_vertices;
-        vertice_2 = rand() % grafo->n_vertices;
+        vertice_1 = rand() % g->n_vertices;
+        vertice_2 = rand() % g->n_vertices;
 
         // Testando para ver se eles já não estão ligados
-        if(is_adjacent_to(vertice1, grafo->adjacency_list[vertice_2]))
+        if(is_adjacent_to(vertice1, g->adjacency_list[vertice_2]))
         {
             continue;  // Se um é adjacente do outro, o outro é adjacente do um, então pegamos um novo par de valores.
         }
 
         // Adicionando o vertice 1 como adjacente do 2
-        add_adjacent(vertice_1, vertice_1, random_weight(max_weight), grafo->adjacency_list[vertice_2]);
+        add_adjacent(vertice_1, vertice_1, random_weight(max_weight), g->adjacency_list[vertice_2]);
         // E adicionando o 2 como adjacente do 1
-        add_adjacent(vertice_2, vertice_2, random_weight(max_weight), grafo->adjacency_list[vertice_1]);
+        add_adjacent(vertice_2, vertice_2, random_weight(max_weight), g->adjacency_list[vertice_1]);
     }
 
+}
+
+void show_graph(graph g)
+{
+    printf("Graph Report\n");
+    printf("\tGraph Metadata\n");
+    printf("\t Vertices: %d\n",g->n_vertices);
+    printf("\t Links: %d\n",g->n_links);
+    printf("Adjacency list\n");
+    int i;
+    for(i = 0; i < g->n_vertices; i++)
+    {
+        printf("\tNode %d:\n",g->adjacency_list[i]->label);
+        list_item current = create_item();
+        for(current = g->adjacency_list[i]->start; current != NULL; current = current->next)
+        {
+            print("\t\t#%d: (%d,%d,%d)\n",k+1,current->label, current->value, current->weight);
+        }
+    }
 }
